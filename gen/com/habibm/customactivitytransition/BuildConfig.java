f4edf6757b351adb6ec39a6bd1703b0905f75a1b
programoo <mevtwifu@gmail.com>
2013-09-01 00:56:42 +0700

    commit

---
+++ gen/com/habibm/customactivitytransition/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.habibm.customactivitytransition;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
